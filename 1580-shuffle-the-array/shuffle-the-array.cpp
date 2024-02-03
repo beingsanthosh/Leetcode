f0de6f54d2c555301c41ba2ec5f3fc75112dd563
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-02-03 07:57:34 +0530

    Time: 4 ms (60.04%) | Memory: 12.1 MB (11.82%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>vet;
+        for(int i=0;i<n;i++)
+        {
+            vet.push_back(nums[i]);
+            vet.push_back(nums[i+n]);
+        }
+        return vet;
+    }
+};
