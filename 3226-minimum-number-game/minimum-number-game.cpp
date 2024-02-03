8e563d3aef05e17031019b9fb8e08c85e4163162
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-02-03 17:13:50 +0530

    Time: 4 ms (81.62%) | Memory: 27.3 MB (5.61%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
-        vector<int>vet;
         sort(nums.begin(),nums.end());
-        int i=0;
-        int n=nums.size();
-        while(i<n)
+        for(int i=1;i<nums.size();i+=2)
         {
-            vet.push_back(nums[i+1]);
-            vet.push_back(nums[i]);
-            i+=2;
+            swap(nums[i],nums[i-1]);
         }
-        return vet;
+        return nums;
     }
 };
