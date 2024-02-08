eaea157f5c661852b18076ce37c7e8f3317f43e9
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-02-08 19:48:06 +0530

    Time: 18 ms (15.47%) | Memory: 10.3 MB (38.03%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<int>freq(128,0);
+        for(auto i:s)
+        {
+            freq[i]++;
+        }
+        auto cmp=[&](char a,char b)
+        {
+            if(freq[a]==freq[b]) return a<b;
+            return freq[a]>freq[b];
+        };
+        sort(s.begin(),s.end(),cmp);
+        return s;
+    }
+};
