ccb07099a9c80beb4a1fd3c33bfb3a2ad3cf12c3
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-02-09 18:45:16 +0530

    Time: 121 ms (96.60%) | Memory: 131.8 MB (5.97%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n==0) return 0;
+        vector<int>vet(n+1,0);
+        vet[0]=0;
+        vet[1]=0;
+        for(int i=2;i<=n;i++)
+        {
+            vet[i]=1;
+        }
+        for(int i=2;i*i<=n;i++)
+        {
+            if(vet[i]==1)
+            {
+                for(int j=i*i;j<=n;j+=i)
+                {
+                    vet[j]=0;
+                }
+            }
+        }
+        int c=0;
+        for(int i=1;i<n;i++)
+        {
+            c+=vet[i];
+        }
+        return c;
+    }
+};
