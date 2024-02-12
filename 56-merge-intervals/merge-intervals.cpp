f581b70d31248ff2e6f402fd36007ea5167ac708
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-02-12 23:33:48 +0530

    Time: 19 ms (94.32%) | Memory: 22.7 MB (30.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
         int n=arr.size();
-        vector<vector<int>>vet;
         sort(arr.begin(),arr.end());
+        vector<vector<int>>ans;
         for(int i=0;i<n;i++)
         {
-            int s=arr[i][0];
-            int e=arr[i][1];
-            if(!vet.empty() and e<=vet.back()[1])
+            if(ans.empty() || ans.back()[1]<arr[i][0])
             {
-                continue;
+                ans.push_back(arr[i]);
             }
-            for(int j=i+1;j<n;j++)
-            {
-                if(arr[j][0]<=e)
-                {
-                    e=max(e,arr[j][1]);
-                }
-                else
-                {
-                    break;
-                }
-            }
-            vet.push_back({s,e});
-        }
-        return vet;
+            else{
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
 
+            }
+        }
+        return ans;
     }
 };
