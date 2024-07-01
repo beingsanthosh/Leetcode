5f9190d8375f875bd57fbad8e3c481133f726dc2
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-07-01 14:51:05 +0530

    Time: 5 ms (49.88%) | Memory: 9.8 MB (9.43%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int count(int n)
+    int bits(int n)
     {
         int c=0;
         while(n)
         {
-            if(n&1)
-            {
-                c++;
-            }
-            n=n>>1;
+        if(n&1)
+        {
+            c++;
+        }
+        n=n>>1;
         }
         return c;
     }
@@ -17,8 +17,9 @@ public:
         vector<int>vet;
         for(int i=0;i<=n;i++)
         {
-            vet.push_back(count(i));
+            vet.push_back(bits(i));
         }
         return vet;
+
     }
 };
