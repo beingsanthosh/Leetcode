e782bb15c6e0a7f443df82fc4185394bf8bb0831
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-07-06 12:00:40 +0530

    Time: 18 ms (65.44%) | Memory: 24.6 MB (85.64%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
+    int singleNonDuplicate(vector<int>& arr) {
+        int n=arr.size();
         int x=0;
-        int n=nums.size();
         for(int i=0;i<n;i++)
         {
-            x=x^nums[i];
+            x=x^arr[i];
         }
         return x;
     }
