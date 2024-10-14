60af52b5c4984c24e25177630c97373cb7c0515c
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-10-14 11:21:20 +0530

    Time: 3 ms (89.52%) | Memory: 16.9 MB (5.25%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,57 +1,57 @@
+int first(vector<int>arr,int t)
+{
+    int n=arr.size();
+    int l=0,r=n-1;
+    int ans=-1;
+    while(l<=r)
+    {
+        int m=(l+r)/2;
+        if(arr[m]==t)
+        {
+            r=m-1;
+            ans=m;
+        }
+        else if(t>arr[m])
+        {
+            l=m+1;
+        }
+        else
+        {
+            r=m-1;
+        }
+    }
+    return ans;
+}
+int last(vector<int>arr,int t)
+{
+    int n=arr.size();
+    int l=0,r=n-1;
+    int ans=-1;
+    while(l<=r)
+    {
+        int m=(l+r)/2;
+        if(arr[m]==t)
+        {
+            l=m+1;
+            ans=m;
+        }
+        else if(t>arr[m])
+        {
+            l=m+1;
+        }
+        else
+        {
+            r=m-1;
+        }
+    }
+    return ans;
+}
+
 class Solution {
 public:
-    int occ1(vector<int>vet,int t)
-    {
-        int n=vet.size();
-        int l=0,r=n-1,ans=-1;
-        while(l<=r)
-        {
-            int m=(l+r)/2;
-            if(vet[m]==t)
-            {
-                ans=m;
-                r=m-1;
-            }
-            else if(vet[m]<t)
-            {
-                l=m+1;
-            }
-            else
-            {
-                r=m-1;
-            }
-        }
-        return ans;
-    }
-    int occ2(vector<int>vet,int t)
-    {
-        int n=vet.size();
-        int l=0,r=n-1;
-        int ans=-1;
-        while(l<=r)
-        {
-            int m=(l+r)/2;
-            if(vet[m]==t)
-            {
-                ans=m;
-                l=m+1;
-            }
-            else if(vet[m]<t)
-            {
-                l=m+1;
-            }
-            else
-            {
-                r=m-1;
-            }
-        }
-        return ans;
-    }
-    vector<int> searchRange(vector<int>& arr, int t) {
-        int n=arr.size();
-        int l=0,r=n-1;
-        int fir=occ1(arr,t);
-        int sec=occ2(arr,t);
-        return {fir,sec};
+    vector<int> searchRange(vector<int>& nums, int t) {
+        int a=first(nums,t);
+        int b=last(nums,t);
+        return {a,b};
     }
 };
