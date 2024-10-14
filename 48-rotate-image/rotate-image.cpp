6aa3443321a9571f8676489372266a2cd400feba
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-10-14 10:51:04 +0530

    Time: 4 ms (39.18%) | Memory: 8.9 MB (83.69%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,24 +3,18 @@ public:
     void rotate(vector<vector<int>>& arr) {
         int n=arr.size();
         int m=arr[0].size();
-        vector<vector<int>>vet(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<i;j++)
             {
-                vet[j][i]=arr[i][j];
+                swap(arr[i][j],arr[j][i]);
             }
         }
         for(int i=0;i<n;i++)
         {
-            reverse(vet[i].begin(),vet[i].end());
+            reverse(arr[i].begin(),arr[i].end());
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                arr[i][j]=vet[i][j];
-            }
-        }
+
+        
     }
 };
