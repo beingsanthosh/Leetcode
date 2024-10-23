773ca0805435c93c5fd810c63472ff9cbbac1398
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-10-23 07:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (98.84%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> buildArray(vector<int>& nums) {
         int n=nums.size();
         vector<int>vet(n,0);
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
             vet[i]=nums[nums[i]];
         }
