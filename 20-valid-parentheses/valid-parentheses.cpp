b1d14defd3099a7ccd2ab45b3a8a63743311aa66
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-12-08 07:50:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -5,26 +5,24 @@ public:
         stack<char>st;
         for(int i=0;i<n;i++)
         {
-            if(s[i]=='(' or s[i]=='[' or s[i]=='{')
+            if(st.empty())
             {
                 st.push(s[i]);
             }
+            else if(s[i]=='(' or s[i]=='[' or s[i]=='{')
+            {
+                st.push(s[i]);
+            }
+            else if((s[i]==')' and st.top()=='(') or (s[i]==']' and st.top()=='[') or (s[i]=='}' and st.top()=='{'))
+            {
+                st.pop();
+            }
             else
             {
-                if(st.empty())
-                {
-                    return false;
-                }
-                if((s[i]==')' and st.top()=='(') or (s[i]==']' and st.top()=='[') or (s[i]=='}' and st.top()=='{'))
-                {
-                    st.pop();
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
         }
         return st.empty();
-    }
+    }   
+    
 };
