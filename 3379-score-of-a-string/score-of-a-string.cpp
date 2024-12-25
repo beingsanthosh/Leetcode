e99e0bf5013d49927320e91e1b7e22b8c9647ed7
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2024-12-25 13:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (41.06%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int sum=0;
-        for(int i=0;i<s.size()-1;i++)
-        {
-            sum+=abs(int(s[i])-int(s[i+1]));
+        int total = 0;
+        for(int i = 0; i < s.size() - 1; i++) {
+            total += abs(s[i] - s[i + 1]);
         }
-        return sum;
-        
+        return total;
     }
 };
