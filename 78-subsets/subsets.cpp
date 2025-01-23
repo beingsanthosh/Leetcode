4d7519d1bea71ce698862daa4ca3a154fbedf1cf
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2025-01-23 21:54:06 +0530

    Time: 3 ms (33.03%) | Memory: 10.2 MB (41.16%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void fun(int i,vector<int>&ds,vector<int>nums,int n,vector<vector<int>>&ans)
-    {
-        if(i==n)
-        {
-            ans.push_back(ds);
-            return;
-        }
-        ds.push_back(nums[i]);
-        fun(i+1,ds,nums,n,ans);
-        ds.pop_back();
-        fun(i+1,ds,nums,n,ans);
-
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>vet;
         int n=nums.size();
-        vector<int>ds;
-        vector<vector<int>>ans;
-        fun(0,ds,nums,n,ans);
-        return ans;
-
+        for(int i=0;i<(pow(2,n));i++)
+        {
+            vector<int>ans;;
+            for(int j=0;j<n;j++)
+            {
+                if((i)&(1<<j))
+                {
+                    ans.push_back(nums[j]);
+                }
+            }
+            vet.push_back(ans);
+        }
+        return vet;
 
     }
 };
