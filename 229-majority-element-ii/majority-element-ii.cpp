1e20e8c1e2dbd1367e517da505441ad5f6265f0f
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2025-01-23 10:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (59.45%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& arr) {
-        int n=arr.size();
-        int c1=0,c2=0,e1,e2;
+    vector<int> majorityElement(vector<int>& nums) {
+        map<int,int>mp;
+        int n=nums.size();
         for(int i=0;i<n;i++)
         {
-            if(c1==0 and arr[i]!=e2)
-            {
-                c1=1;
-                e1=arr[i];
-            }
-            else if(c2==0 and arr[i]!=e1)
-            {
-                c2=1;
-                e2=arr[i];
-            }
-            else if(arr[i]==e1)
-            {
-                c1++;
-            }
-            else if(arr[i]==e2)
-            {
-                c2++;
-            }
-            else
-            {
-                c1--;
-                c2--;
-            }
+            mp[nums[i]]++;
         }
-        vector<int>vet;
-        int m=(int)(n/3)+1;
-        int a=0,b=0;
-        for(int i=0;i<n;i++)
+        vector<int>ans;
+        for(auto i:mp)
         {
-            if(arr[i]==e1) a++;
-            if(arr[i]==e2) b++;
+            if(i.second>(n/3))
+            {
+                ans.push_back(i.first);
+            }
         }
-        if(e1==e2)
-        {
-            return {e1};
-        }
-        if(a>=m) vet.push_back(e1);
-        if(b>=m) vet.push_back(e2);
-        return vet;
+        return ans;
     }
 };
