09347cb2ce72f1df9d429c1dc047b59a0d24df48
santhosh ramisetti <139674269+beingsanthosh@users.noreply.github.com>
2025-01-23 08:25:42 +0530

    Time: 567 ms (29.61%) | Memory: 295.7 MB (7.16%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,47 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums;
+    void merge(vector<int>&arr,int l,int m,int r)
+    {
+        vector<int>temp;
+        int left=l;
+        int right=m+1;
+        while (left <= m && right <= r) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= m) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= r) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = l; i <=r; i++) {
+        arr[i] = temp[i - l];
+    }
+
+    }
+    void mergesort(vector<int> &arr,int l,int r)
+    {
+        if(l>=r) return;
+        int m=(l+r)/2;
+        mergesort(arr,l,m);
+        mergesort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+    vector<int> sortArray(vector<int>& arr) {
+        int n=arr.size();
+        int l=0,r=n-1;
+        mergesort(arr,l,r);
+        return arr;
     }
 };
